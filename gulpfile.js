04e973bdc4fa4e4ba4b721954d05ed7dd8550bb8
afeiship <1290657123@qq.com>
2018-01-26 14:01:46 +0800

    issue: sass/scss support json import

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+(function () {
+
+    'use strict';
+
+    var gulp = require('gulp');
+    var $ = require('gulp-load-plugins')({
+        pattern: ['gulp-*', 'gulp.*', 'del']
+    });
+
+    //clean
+    gulp.task('clean', function () {
+        return $.del('dist');
+    });
+
+    gulp.task('styles', function () {
+        return gulp
+            .src('src/**/*.sagss')
+            .pipe($.sassImportJson({isScss: false}))
+            .pipe($.sass({outputStyle: 'expanded'}).on('error', $.sass.logError))
+            .pipe(gulp.dest('dist'));
+    });
+
+    gulp.task('default', ['clean'], function () {
+        gulp.start(['styles'])
+    });
+
+}());
